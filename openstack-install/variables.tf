f3e85bc99f252fdcce999fb3d74fca4250a8c410
José Domingo <josedom24@gmail.com>
2017-03-09 09:28:29 +0100

    variables2

--- openstack-install/variables.tf
+++ openstack-install/variables.tf
@@ -2,9 +2,9 @@ variable "secret_key" {}
 
 variable "ext-net" { default = "ext-net"}
 
-variable "ip_subred-ext" {default = "192.168.200.0/24"}
+variable "ip_subred-ext" {default = "192.168.1.0/24"}
 variable "dns_subred-ext" { type="list" default=["192.168.102.2"]}
-variable "ip_subred-int" {default = "192.168.201.0/24"}
+variable "ip_subred-int" {default = "192.168.221.0/24"}
 
 variable "imagen" {default = "ca6effe7-0a47-4257-8624-713225a0bbdd"}
 variable "sabor" {default = "13"}
