4ad27bab447cc5980eebef7fadcfa74e4269527b
Jose Domingo <jose@debian.gonzalonazareno.org>
2017-03-10 11:05:46 +0100

    ubuntu

--- openstack-install/demo-openrc.sh
+++ openstack-install/demo-openrc.sh
@@ -1,5 +1,4 @@
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 # To use an OpenStack cloud you need to authenticate against the Identity
 # service named keystone, which returns a **Token** and **Service Catalog**.
@@ -8,24 +7,29 @@
 # Storage, and Networking (code-named nova, glance, keystone, swift,
 # cinder, and neutron).
 #
-# *NOTE*: Using the 2.0 *Identity API* does not necessarily mean any other
-# OpenStack API is version 2.0. For example, your cloud provider may implement
+# *NOTE*: Using the 3 *Identity API* does not necessarily mean any other
+# OpenStack API is version 3. For example, your cloud provider may implement
 # Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
 # only for the Identity API served through keystone.
-export OS_AUTH_URL=http://149.202.199.10:5000/v2.0
+export OS_AUTH_URL=https://jupiter.gonzalonazareno.org:5000/v3
 
-# With the addition of Keystone we have standardized on the term **tenant**
+# With the addition of Keystone we have standardized on the term **project**
 # as the entity that owns the resources.
-export OS_TENANT_ID=f7d3bee6f6744f499918cfbd3f8780a2
-export OS_TENANT_NAME="demo"
+export OS_PROJECT_ID=639d2d75b1bc4aa082d33872a09591d6
 export OS_PROJECT_NAME="demo"
+export OS_USER_DOMAIN_NAME="default"
+if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
+
+# unset v2.0 items in case set
+unset OS_TENANT_ID
+unset OS_TENANT_NAME
 
 # In addition to the owning entity (tenant), OpenStack stores the entity
 # performing the action as the **user**.
 export OS_USERNAME="demo"
 
 # With Keystone you pass the keystone password.
-echo "Please enter your OpenStack Password: "
+echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
 read -sr OS_PASSWORD_INPUT
 export OS_PASSWORD=$OS_PASSWORD_INPUT
 
@@ -34,3 +38,6 @@ export OS_PASSWORD=$OS_PASSWORD_INPUT
 export OS_REGION_NAME="RegionOne"
 # Don't leave a blank variable, unset it if it was empty
 if [ -z "$OS_REGION_NAME" ]; then unset OS_REGION_NAME; fi
+
+export OS_INTERFACE=public
+export OS_IDENTITY_API_VERSION=3
